f2586c6cf883a315b0fe3b2b76f269e3eb2e5a41
key <412391882@qq.com>
2019-07-01 10:35:03 +0800

    Update and rename Json2Dict.py to JSONandHTTPP.py

--- JSONandHTTPP.py
+++ JSONandHTTPP.py
@@ -1,7 +1,7 @@
-#-*- coding:utf-8 -*-
-#Auther: Vulkey_Chen
-#Blog: gh0st.cn
-#Team: MSTSEC
+# -*- coding:utf-8 -*-
+# Auther: Vulkey_Chen
+# Blog: gh0st.cn
+# Team: MSTSEC
 
 import json,re
 
@@ -12,10 +12,10 @@ from java.awt import BorderLayout
 class BurpExtender(IBurpExtender, ITab):
 
     def registerExtenderCallbacks(self, callbacks):
-        print 'Json2Dict by [Vulkey_Chen]\nBlog: gh0st.cn\nTeam: MSTSEC'
+        print 'JSONP&HTTPP by [Vulkey_Chen]\nBlog: gh0st.cn\nTeam: MSTSEC'
         self._cb = callbacks
         self._hp = callbacks.getHelpers()
-        self._cb.setExtensionName('Json2Dict')
+        self._cb.setExtensionName('JSONP&HTTPP')
         self.mainPanel = JPanel()
         self.mainPanel.setLayout(BorderLayout())
 
@@ -39,25 +39,35 @@ class BurpExtender(IBurpExtender, ITab):
         self.mainPanel.add(self.jsonTextWrapper, BorderLayout.NORTH)
         self.mainPanel.add(self.dictTextWrapper, BorderLayout.CENTER)
 
-        self.beautifyButton = JButton("Convert", actionPerformed=self.onClick)
+        self.beautifyButton_1 = JButton("JSON2HTTPP", actionPerformed=self.onClick1)
+        self.beautifyButton_2 = JButton("HTTPP2JSON", actionPerformed=self.onClick2)
 
         self.buttons = JPanel();
-        self.buttons.add(self.beautifyButton, BorderLayout.CENTER)
+        self.buttons.add(self.beautifyButton_1, BorderLayout.CENTER)
+        self.buttons.add(self.beautifyButton_2, BorderLayout.CENTER)
 
         self.mainPanel.add(self.buttons, BorderLayout.SOUTH)
 
         self._cb.customizeUiComponent(self.mainPanel)
         self._cb.addSuiteTab(self)
 
-    def onClick(self, event):
+    def onClick1(self, event):
         _jsontext = self.jsonTextArea.getText()
         try:
             _jsontext = json.loads(re.search(r'\({.*?}\)',_jsontext).group().replace('(','').replace(')',''))
         except:
             _jsontext = json.loads(_jsontext)
-        self._resul = []
-        # self.dictTextArea.setText('&'.join(self.json2dict(_jsontext)))
-        self.dictTextArea.setText('\n'.join(self.json2dict(_jsontext)))
+        self._result = []
+        self.dictTextArea.setText('&'.join(self.json2dict(_jsontext)))
+        # self.dictTextArea.setText('\n'.join(self.json2dict(_jsontext)))
+
+    def onClick2(self, event):
+        _jsontext = self.jsonTextArea.getText()
+        _res = []
+        for i in _jsontext.split('&'):
+            for x in i.split('='):
+                _res.append(x)
+        self.dictTextArea.setText(json.dumps(dict(zip(_res[0::2],_res[1::2]))))
 
     def json2dict(self,_jsontext):
         keyValue = ""
@@ -71,20 +81,20 @@ class BurpExtender(IBurpExtender, ITab):
                         self.json2dict(json_array)
                 else:
                     if type(keyValue) is int or type(keyValue) == long or type(keyValue) == str:
-                        self._resul.append(str(key) + "=" + str(keyValue))
+                        self._result.append(str(key) + "=" + str(keyValue))
                     elif type(keyValue) is bool:
-                        self._resul.append(str(key) + "=" + str(int(keyValue)))
+                        self._result.append(str(key) + "=" + str(int(keyValue)))
                     elif type(keyValue) == type(None):
-                        self._resul.append(str(key) + "=" + "")
+                        self._result.append(str(key) + "=" + "")
                     else:
-                        self._resul.append(str(key) + "=" + keyValue)
+                        self._result.append(str(key) + "=" + keyValue)
         elif isinstance(_jsontext, list):
             for _jsontext_array in _jsontext:
                 self.json2dict(_jsontext_array)
-        return self._resul
+        return self._result
 
     def getTabCaption(self):
-        return 'Json2Dict'
+        return 'JSONP&HTTPP'
 
     def getUiComponent(self):
         return self.mainPanel
